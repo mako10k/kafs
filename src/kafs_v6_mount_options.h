5bfb0cca0f28ad702413fb442ee0e9275d69df7d
Makoto Katsumata <mako10k@mk10.org>
2026-07-20 09:12:16 +0900

    refactor: share mount option filtering
    Extract neutral FUSE argv filtering, thread-option parsing, legacy-token rejection, and owned-argument cleanup while retaining format-specific v6/v7 runtime-token classification.

--- src/kafs_v6_mount_options.h
+++ src/kafs_v6_mount_options.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "kafs.h"
+#include "kafs_mount_options_common.h"
 #include "kafs_v6_runtime.h"
 
 #include <stdio.h>
@@ -14,13 +15,7 @@
  * has one owner.
  */
 
-typedef struct kafs_v6_mount_thread_options
-{
-  kafs_bool_t enable_mt;
-  int saw_max_threads;
-  unsigned mt_cnt_override;
-  int mt_cnt_override_set;
-} kafs_v6_mount_thread_options_t;
+typedef kafs_mount_thread_options_t kafs_v6_mount_thread_options_t;
 
 int kafs_v6_mount_options_record_runtime_token(kafs_v6_runtime_request_t *req, const char *tok);
 int kafs_v6_mount_options_filter_fuse_args(const char *mountpoint, int argc_extra,
